0fb4ce88dd53cb24af1856a24d60e7625540e1dd
ajayanbu5322-prog <ajayanbu5322@gmail.com>
2025-10-05 12:19:58 +0530

    created

--- src/Common/ProjectCard.jsx
+++ src/Common/ProjectCard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function ProjectCard({src, h3, p}) {
+function ProjectCard({src, h3, p, Link}) {
   return (
-    <a href="" target="-blank">
+    <a href={Link} target="-blank">
       <img className="hover" src={src} alt={`${h3}`} />
       <h3>{h3}</h3>
       <p>{p}</p>
--- src/Section/Projects/Projects.jsx
+++ src/Section/Projects/Projects.jsx
@@ -12,7 +12,7 @@ function Projects() {
     <section id='projects' className={styles.container}>
         <h1 className='sectionTitle'>Projects</h1>
         <div className={styles.projectsContainer}>
-            <ProjectCard src={viberr} h3="Viberr" p="Streaming App"/>
+            <ProjectCard src={viberr} h3="E Commerce-Website" p="E Commerce App" Link="https://github.com/ajayanbu5322-prog/E-Commerce-Website"/>
             <ProjectCard src={freshBurger} h3="Fresh Burger" p="KFC Restaurant"/>
             <ProjectCard src={hipsster} h3="Hipsster" p="Glasses Shop"/>
             <ProjectCard src={fitLift} h3="FitLift" p="Fitness App"/>
